025cbc3392ba67bf92e413b72a8f9c71701da887
Brandon328 <70345941@continental.edu.pe>
2023-04-27 14:55:51 -0500

    Componentes de navegación con ReactRouter

---
+++ src/components/Menu.jsx
@@ -0,0 +1,7 @@
+function Menu() {
+  return (
+    <p>Menu</p>
+  )
+}
+
+export { Menu }
---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,7 @@
+function ProfilePage() {
+  return (
+    <h1>ProfilePage</h1>
+  )
+}
+
+export { ProfilePage }
