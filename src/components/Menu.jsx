18d743893861f757011270a5d740087a2c04e348
Brandon328 <70345941@continental.edu.pe>
2023-04-27 16:01:51 -0500

    NavLink

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,43 @@
+import { NavLink } from "react-router-dom"
+
 function Menu() {
   return (
-    <p>Menu</p>
+    <nav>
+      <ul>
+        {
+          routes.map((route, index) =>
+            <li
+              key={index}
+            >
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? 'red' : 'blue'
+                })}
+                to={route.to}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          )
+        }
+      </ul>
+    </nav>
   )
 }
+
+const routes = [
+  {
+    to: '/',
+    text: 'home'
+  },
+  {
+    to: '/blog',
+    text: 'blog'
+  },
+  {
+    to: '/profile',
+    text: 'profile'
+  },
+]
 
 export { Menu }
